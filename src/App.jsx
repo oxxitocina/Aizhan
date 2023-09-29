f2f4287f24dd485d9070b30ed9a70885ec07f862
oxxitocina <U2HDxO@protonmail.com>
2023-09-29 16:57:28 +0600

    add logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,77 @@
 import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [ score, setScore ] = useState({
+    userScore: 0,
+    compScore: 0
+  })
+  
+  function convertToWord(n){
+    if(n === 0){ return 'Rock'}
+    if(n === 1){ return 'Paper'}
+    if(n === 2){return 'Scissors'}
+    else{return "Computer's choice"}
+
+  }
+
+  function updateScore() {
+    
+  }
+
+  function win(userChoice, compChoice){
+    result_p.innerHTML = convertToWord(userChoice) + ' beats ' + convertToWord(compChoice) + ". You win!";
+  }
+
+  function lose(userChoice, compChoice){
+  result_p.innerHTML = convertToWord(userChoice) + ' loses to ' + convertToWord(compChoice) + ". You lost...";
+  }
+
+  function draw(userChoice, compChoice){
+    result_p.innerHTML = convertToWord(userChoice) + ' equals to ' + convertToWord(compChoice) + ". It's a draw!";
+  }
+
+  async function game(userChoice){
+
+    const bet = document.getElementById('bet').value;
+
+    const value = ethers.utils.parseEther(bet);
+
+    // Unscribe, why not work((((
+    await contract.off("GamePlayed");
+    // userScore_span.removeEventListener('DOMSubtreeModified', updateScore);
+    // compScore_span.removeEventListener('DOMSubtreeModified', updateScore);
+
+    // Subscribe
+    const eventName = "GamePlayed";
+    await contract.on(eventName, (player, isWinner, outcome, userChoice, compChoice, event) => {
+        console.log(`Player: ${player}, Winner: ${isWinner}, Outcome: ${outcome}`);
+        const compChoiceInt = parseInt(compChoice.toString());
+        if(outcome === "TIE"){draw(userChoice, compChoiceInt)}
+        else if(outcome === "WIN"){win(userChoice, compChoiceInt)}
+        else if(outcome === "LOSE"){lose(userChoice, compChoiceInt)}
+    });
+
+    await contract.playgame(userChoice,  { value: value });
+
+}
+
+async function viewHistory(){
+    const eventName = "GamePlayed";
+    const filter = contract.filters.GamePlayed();
+
+    const events = await contract.queryFilter(filter);
+
+    events.forEach((event) => {
+        const player = event.args.player;
+        const isWinner = event.args.isWinner;
+        const outcome = event.args.outcome;
+        const userChoice = event.args.userChoice;
+        const compChoice = parseInt(event.args.compChoice.toString());
+        
+        history.innerHTML +=  `<br> Player: ${player}, Winner: ${isWinner}, Outcome: ${outcome} <br>Where User choice - ${convertToWord(userChoice)}, Computer choice - ${convertToWord(compChoice)}<br><br>`;
+    });
+}
 
   return (
     <>
@@ -19,13 +89,13 @@ function App() {
     </div>
 
     <div className="choices">
-        <div className="choice" id="r">
+        <div className="choice" id="r" onClick={() => {game(0)}}>
             <img src="rock.png" alt="" style={{width: '350px'}}/>
         </div>
-        <div className="choice" id="p">
+        <div className="choice" id="p" onClick={() => {game(1)}}>
             <img src="paper.png" alt="" style={{width: '350px'}}/>
         </div>
-        <div className="choice" id="s">
+        <div className="choice" id="s" onClick={() => {game(2)}}>
             <img src="scissors.png" alt="" style={{width: '350px'}}/>
         </div>
     </div>
@@ -33,7 +103,7 @@ function App() {
     <p id="action-msg">Make your choice.</p>
     
     <div className="gamehistory">
-        <button>Game History</button> <p style={{fontSize: 'x-small'}}>if not shown, try click twice or wait longer</p>
+        <button onClick={viewHistory}>Game History</button> <p style={{fontSize: 'x-small'}}>if not shown, try click twice or wait longer</p>
         <p><span id="history"></span></p>
     </div>
     </>
